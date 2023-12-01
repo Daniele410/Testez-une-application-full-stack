99da84e91f86970a40bcf4e37296d22beaca8a1d
Daniele410 <miraglia.daniele410@gmail.com>
2023-12-01 17:53:41 +0100

    add test userController

--- back/README.md
+++ back/README.md
@@ -1,7 +1,40 @@
-# Yoga App !
+## Introduction
 
+This application serves as a straightforward tool for managing yoga classes. It streamlines scheduling, attendance
+tracking, and student progress monitoring. The application is developed using Java 8.
 
-For launch and generate the jacoco code coverage:
-> mvn clean test
+## Java Version Configuration
 
-GL
+For Windows users, modifying the Java version utilized by the application involves the following steps:
+
+1. Open the Command Prompt.
+
+2. Execute the following command to display the current Java version:
+    ```cmd
+    echo %JAVA_HOME%
+    ```
+
+3. Set the JAVA_HOME environment variable to the path of your desired Java version.
+
+   For instance, to use Java 11, execute the following command:
+    ```cmd
+    set JAVA_HOME=C:\Program Files\Java\jdk-11.0.12
+    ```
+   Replace `jdk-11.0.12` with the actual version number of your Java installation.
+
+4. Close the Command Prompt.
+
+## Generating Code Coverage Report
+
+To generate the Jacoco code coverage report, follow these steps:
+
+1. Ensure Maven is installed on your system.
+
+2. Open a terminal and navigate to the project directory.
+
+3. Execute the following command:
+    ```cmd
+    mvn clean test
+    ```
+   This command compiles the project, executes the unit tests, and generates the Jacoco code coverage report. The report
+   is located in the `target` directory under the name `jacoco-it.report`.
---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -0,0 +1,171 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import com.openclassrooms.starterjwt.dto.UserDto;
+import com.openclassrooms.starterjwt.mapper.UserMapper;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.services.UserService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+    @Mock
+    private UserMapper userMapper;
+    @Mock
+    private UserService userService;
+    @Mock
+    Authentication authentication;
+    @Mock
+    SecurityContext securityContext;
+    @Mock
+    UserDetails userDetails;
+    @InjectMocks
+    private UserController userController;
+
+
+    @Test
+    public void testFindById_shouldReturnResponseOk() {
+        // given
+        long userId = 1L;
+        User mockUser = User.builder()
+                .id(1L)
+                .firstName("Mario")
+                .lastName("Rossi")
+                .password("password")
+                .email("mario@mail.com")
+                .build();
+
+        UserDto mockUserDto = new UserDto();
+        mockUserDto.setId(1L);
+        mockUserDto.setFirstName("Mario");
+
+        when(userService.findById(1L)).thenReturn(mockUser);
+        when(userMapper.toDto(mockUser)).thenReturn(mockUserDto);
+
+        // when
+        ResponseEntity<?> responseEntity = userController.findById(String.valueOf(userId));
+
+        // then
+        assertEquals(200, responseEntity.getStatusCodeValue());
+        assertEquals(mockUser.getFirstName(), mockUserDto.getFirstName());
+    }
+
+    @Test
+    public void findById_shouldReturnResponseNotFoundCode() {
+        // given
+        long userId = 2L;
+        UserDto mockUserDto = new UserDto();
+        mockUserDto.setId(1L);
+        mockUserDto.setFirstName("Mario");
+
+        when(userService.findById(2L)).thenReturn(null);
+
+        // when
+        ResponseEntity<?> responseEntity = userController.findById(String.valueOf(userId));
+
+        // then
+        assertEquals(404, responseEntity.getStatusCodeValue());
+    }
+
+    @Test
+    void findById_InvalidId_shouldReturnBadRequest() {
+        ResponseEntity<?> response = userController.findById("nonNumericId");
+        assertEquals(400, response.getStatusCodeValue());
+    }
+
+
+    @Test
+    void save_ValidId_UserDeletedSuccessfully() {
+        // given
+        long userId = 1L;
+        User user = User.builder()
+                .id(1L)
+                .firstName("Mario")
+                .lastName("Rossi")
+                .password("password")
+                .email("mario@mail.com")
+                .build();
+
+        when(userDetails.getUsername()).thenReturn("mario@mail.com");
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(userService.findById(Long.valueOf(userId))).thenReturn(user);
+
+        // when
+        ResponseEntity<?> response = userController.save(String.valueOf(userId));
+
+        // then
+        assertEquals(200, response.getStatusCodeValue());
+
+    }
+
+    @Test
+    void saveUserNull_shouldReturnResponseNotFound() {
+        // given
+        long userId = 1L;
+
+        when(userService.findById(userId)).thenReturn(null);
+
+        // when
+        ResponseEntity<?> response = userController.save(String.valueOf(userId));
+
+        // then
+        assertEquals(404, response.getStatusCodeValue());
+
+    }
+
+    @Test
+    void saveUserNotAuthenticated_shouldReturnUnauthorizedCode() {
+        // given
+        long userId = 1L;
+        User user = User.builder()
+                .id(1L)
+                .firstName("Mario")
+                .lastName("Rossi")
+                .password("password")
+                .email("mario@mail.com")
+                .build();
+
+        when(userDetails.getUsername()).thenReturn("test@mail.com");
+        when(authentication.getPrincipal()).thenReturn(userDetails);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+        when(userService.findById(Long.valueOf(userId))).thenReturn(user);
+
+        // when
+        ResponseEntity<?> response = userController.save(String.valueOf(userId));
+
+        // then
+        assertEquals(401, response.getStatusCodeValue());
+
+    }
+
+    @Test
+    void saveUserNotAuthenticated_shouldReturnBadRequestCode() {
+        // given
+        String userIdString = "nonNumericId";
+
+        // when
+        ResponseEntity<?> response = userController.save(userIdString);
+
+        // then
+        assertEquals(400, response.getStatusCodeValue());
+
+    }
+}
